017533a9b76128f31ffda31621960ccb7e2365ab
riyandrmd <ryanda.rama10@gmail.com>
2024-08-14 21:26:24 +0700

    create navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-expand-lg text-light bg-primary">
+    <div class="container-fluid">
+      <div class="d-flex align-items-center justify-content-center">
+        <a class="navbar-brand mt-2 mt-lg-0" href="#">
+          <img
+            src="@/assets/pln_icon.png"
+            height="50"
+            alt="pln logo"
+            loading="lazy"
+          />
+        </a>
+        <h1 class="fs-6 ms-2 mb-0">iMeeting</h1>
+      </div>
+
+      <div class="d-flex align-items-center gap-2">
+        <a class="link-secondary me-3" href="#">
+          <i class="bi bi-bell text-light"></i>
+        </a>
+        <div class="d-flex align-items-center justify-content-center">
+          <a class="mt-2 mt-lg-0" href="#">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+              class="rounded-circle"
+              height="40"
+              alt="Avatar"
+              loading="lazy"
+            />
+          </a>
+          <h1 class="dropdown-toggle fs-6 ms-2 mb-0">Jhon Doe</h1>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
